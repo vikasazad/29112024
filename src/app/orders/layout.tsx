d2857e2c4e062bbd85be7764ef5132b4bf6252a6
vikasazad <vikumar.azad@gmail.com>
2024-11-29 18:32:19 +0530

    29/11/2024

---
+++ src/app/orders/layout.tsx
@@ -0,0 +1,20 @@
+import { Header } from "@/components/header";
+import { Metadata } from "next";
+
+// app/dashboard/layout.tsx
+export const metadata: Metadata = {
+  title: "Oders",
+  description: "Oders",
+};
+export default function OdersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      {children}
+    </div>
+  );
+}
